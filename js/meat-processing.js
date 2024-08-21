d54077520810f8dcfedc2a18bb45d63c9583f08a
fuadsadiqov <sadiqovfuad8@gmail.com>
2024-08-22 01:01:27 +0400

    add some change

---
+++ js/meat-processing.js
@@ -0,0 +1,102 @@
+const companies = [
+  {
+    id: 1,
+    title: "SAB",
+    description: {
+      az: `
+      Yerli şirkət olan “Zəhmət-Ruzi” Məhdud Məsuliyyətli Cəmiyyəti 1998-ci ildən fəaliyyət göstərir. Hazırda “Zəhmət-Ruzi” MMC 3 hektardan artıq ərazidə yerləşir.
+      <br> <br>
+      Ən müasir avadanlıqlarla təchiz edilmiş laboratoriyalar, istehsal sexləri, anbar və daşınma sistemləri. “Zəhmət-Ruzi” ailəsində bu gün müxtəlif sahələr üzrə 150-yə yaxın yüksək ixtisaslı mütəxəssis çalışır.`,
+      en: `
+        The local company "Zəhmət-Ruzi" LLC has been operating since 1998. Currently, "Zəhmət-Ruzi" LLC is located on an area of more than 3 hectares.
+        <br> <br>
+        The company features laboratories, production workshops, warehouses, and transportation systems equipped with the most modern equipment. Today, around 150 highly qualified specialists work across various fields within the "Zəhmət-Ruzi" family.`,
+    },
+    image: "/assets/images/meat-processing/sab-banner.png",
+  },
+  {
+    id: 2,
+    title: "MR.FOOD",
+    description: {
+      az: `
+      Yerli şirkət olan “Zəhmət-Ruzi” Məhdud Məsuliyyətli Cəmiyyəti 1998-ci ildən fəaliyyət göstərir. Hazırda “Zəhmət-Ruzi” MMC 3 hektardan artıq ərazidə yerləşir.
+      <br> <br>
+      Ən müasir avadanlıqlarla təchiz edilmiş laboratoriyalar, istehsal sexləri, anbar və daşınma sistemləri. “Zəhmət-Ruzi” ailəsində bu gün müxtəlif sahələr üzrə 150-yə yaxın yüksək ixtisaslı mütəxəssis çalışır.`,
+      en: `
+        The local company "Zəhmət-Ruzi" LLC has been operating since 1998. Currently, "Zəhmət-Ruzi" LLC is located on an area of more than 3 hectares.
+        <br> <br>
+        The company features laboratories, production workshops, warehouses, and transportation systems equipped with the most modern equipment. Today, around 150 highly qualified specialists work across various fields within the "Zəhmət-Ruzi" family.`,
+    },
+    image: "/assets/images/meat-processing/sab-banner.png",
+  },
+  {
+    id: 3,
+    title: "ÜMİD",
+    description: {
+      az: `
+      Yerli şirkət olan “Zəhmət-Ruzi” Məhdud Məsuliyyətli Cəmiyyəti 1998-ci ildən fəaliyyət göstərir. Hazırda “Zəhmət-Ruzi” MMC 3 hektardan artıq ərazidə yerləşir.
+      <br> <br>
+      Ən müasir avadanlıqlarla təchiz edilmiş laboratoriyalar, istehsal sexləri, anbar və daşınma sistemləri. “Zəhmət-Ruzi” ailəsində bu gün müxtəlif sahələr üzrə 150-yə yaxın yüksək ixtisaslı mütəxəssis çalışır.`,
+      en: `
+        The local company "Zəhmət-Ruzi" LLC has been operating since 1998. Currently, "Zəhmət-Ruzi" LLC is located on an area of more than 3 hectares.
+        <br> <br>
+        The company features laboratories, production workshops, warehouses, and transportation systems equipped with the most modern equipment. Today, around 150 highly qualified specialists work across various fields within the "Zəhmət-Ruzi" family.`,
+    },
+    image: "/assets/images/meat-processing/sab-banner.png",
+  },
+  {
+    id: 4,
+    title: "KÖVSƏR",
+    description: {
+      az: `
+      Yerli şirkət olan “Zəhmət-Ruzi” Məhdud Məsuliyyətli Cəmiyyəti 1998-ci ildən fəaliyyət göstərir. Hazırda “Zəhmət-Ruzi” MMC 3 hektardan artıq ərazidə yerləşir.
+      <br> <br>
+      Ən müasir avadanlıqlarla təchiz edilmiş laboratoriyalar, istehsal sexləri, anbar və daşınma sistemləri. “Zəhmət-Ruzi” ailəsində bu gün müxtəlif sahələr üzrə 150-yə yaxın yüksək ixtisaslı mütəxəssis çalışır.`,
+      en: `
+        The local company "Zəhmət-Ruzi" LLC has been operating since 1998. Currently, "Zəhmət-Ruzi" LLC is located on an area of more than 3 hectares.
+        <br> <br>
+        The company features laboratories, production workshops, warehouses, and transportation systems equipped with the most modern equipment. Today, around 150 highly qualified specialists work across various fields within the "Zəhmət-Ruzi" family.`,
+    },
+    image: "/assets/images/meat-processing/sab-banner.png",
+  },
+];
+document.addEventListener("DOMContentLoaded", () => {
+  const activeCompany = 1;
+
+  function setCompany(id) {
+    let title = document.getElementById("title");
+    let description = document.getElementById("description");
+    let image = document.getElementById("image");
+    let company = companies.find((c) => c.id === id);
+
+    if (company) {
+      const currentLang = localStorage.getItem("language") || "az";
+      const newLang = currentLang === "en" ? "en" : "az";
+
+      title.textContent = company.title;
+      description.innerHTML = company.description[newLang];
+      image.src = company.image;
+    }
+  }
+
+  setCompany(activeCompany);
+
+  const companyElements = document.querySelectorAll("[data-company]");
+  companyElements.forEach((com) => {
+    com.addEventListener("click", () => {
+      const companyId = parseInt(com.getAttribute("data-company"));
+      companyElements.forEach((c) => c.classList.remove("bg-[#F0F0F0]"));
+      com.classList.add("bg-[#F0F0F0]");
+      setCompany(companyId);
+    });
+  });
+
+  new fullpage("#fullpage", {
+    autoScrolling: true,
+
+    // afterLoad: function (origin, destination, direction) {
+    //   const section = destination.index;
+
+    // },
+  });
+});
