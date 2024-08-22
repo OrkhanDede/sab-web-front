c908382e73f12ff4f02499180b3e859e76bd0edd
fuadsadiqov <sadiqovfuad8@gmail.com>
2024-08-23 00:52:02 +0400

    add map

---
+++ js/packages/mapdata.js
@@ -0,0 +1,473 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "#C0C0C0",
+    state_hover_color: "#EE3239",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "no",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_url: "",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "no",
+    manual_zoom: "no",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "0",
+    initial_zoom_solo: "no",
+    region_opacity: 0,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "no",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    AZE1676: {
+      name: "Ağstafa",
+      description: "Details about Ağstafa",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1677: {
+      name: "Daşkəsən",
+      description: "Details about Daşkəsən",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1678: {
+      name: "Gədəbəy",
+      description: "Details about Gədəbəy",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1679: {
+      name: "Gəncə",
+      description: "Details about Gəncə",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1680: {
+      name: "Xanlar",
+      description: "Details about Xanlar",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1681: {
+      name: "Goranboy",
+      description: "Details about Goranboy",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1682: {
+      name: "Kəlbəcər",
+      description: "Details about Kəlbəcər",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1683: {
+      name: "Mingəçevir",
+      description: "Details about Mingəçevir",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1684: {
+      name: "Qazax",
+      description: "Details about Qazax",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1685: {
+      name: "Şəmkir",
+      description: "Details about Şəmkir",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1686: {
+      name: "Samux",
+      description: "Details about Samux",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1687: {
+      name: "Tovuz",
+      description: "Details about Tovuz",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1688: {
+      name: "Yevlakh Rayon",
+      description: "Details about Yevlakh",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1689: {
+      name: "Bakı",
+      description: "Details about Bakı",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1690: {
+      name: "Abşeron",
+      description: "Details about Abşeron",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1691: {
+      name: "Ağdam",
+      description: "Details about Ağdam",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1692: {
+      name: "Ağdaş",
+      description: "Details about Ağdaş",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1693: {
+      name: "Ağsu",
+      description: "Details about Ağsu",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1694: {
+      name: "Hajigabul",
+      description: "Details about Hajigabul",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1695: {
+      name: "Astara",
+      description: "Details about Astara",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1696: {
+      name: "Bərdə",
+      description: "Details about Bərdə",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1697: {
+      name: "Beyləqan",
+      description: "Details about Beyləqan",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1698: {
+      name: "Biləsuvar",
+      description: "Details about Biləsuvar",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1699: {
+      name: "Cəbrayıl",
+      description: "Details about Cəbrayıl",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1700: {
+      name: "Cəlilabad",
+      description: "Details about Cəlilabad",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1701: {
+      name: "Dəvəçi",
+      description: "Details about Dəvəçi",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1702: {
+      name: "Füzuli",
+      description: "Details about Füzuli",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1703: {
+      name: "Göyçay",
+      description: "Details about Göyçay",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1704: {
+      name: "İmişli",
+      description: "Details about İmişli",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1705: {
+      name: "İsmayıllı",
+      description: "Details about İsmayıllı",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1706: {
+      name: "Kürdəmir",
+      description: "Details about Kürdəmir",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1707: {
+      name: "Lankaran",
+      description: "Details about Lankaran",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1708: {
+      name: "Masallı",
+      description: "Details about Masallı",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1709: {
+      name: "Lerik",
+      description: "Details about Lerik",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1710: {
+      name: "Neftçala",
+      description: "Details about Neftçala",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1711: {
+      name: "Qobustan",
+      description: "Details about Qobustan",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1712: {
+      name: "Siyəzən",
+      description: "Details about Siyəzən",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1713: {
+      name: "Saatlı",
+      description: "Details about Saatlı",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1714: {
+      name: "Sabirabad",
+      description: "Details about Sabirabad",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1715: {
+      name: "Salyan",
+      description: "Details about Salyan",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1716: {
+      name: "Şamaxı",
+      description: "Details about Şamaxı",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1717: {
+      name: "Sumqayıt",
+      description: "Details about Sumqayıt",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1718: {
+      name: "Ucar",
+      description: "Details about Ucar",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1719: {
+      name: "Xizı",
+      description: "Details about Xizı",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1720: {
+      name: "Yardımlı",
+      description: "Details about Yardımlı",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1721: {
+      name: "Zərdab",
+      description: "Details about Zərdab",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1722: {
+      name: "Ağcabədi",
+      description: "Details about Ağcabədi",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1723: {
+      name: "Balakən",
+      description: "Details about Balakən",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1724: {
+      name: "Qəbələ",
+      description: "Details about Qəbələ",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1725: {
+      name: "Oğuz",
+      description: "Details about Oğuz",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1726: {
+      name: "Qax",
+      description: "Details about Qax",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1727: {
+      name: "Şəki",
+      description: "Details about Şəki",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1728: {
+      name: "Quba",
+      description: "Details about Quba",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1729: {
+      name: "Qusar",
+      description: "Details about Qusar",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1730: {
+      name: "Xaçmaz",
+      description: "Details about Xaçmaz",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1731: {
+      name: "Zaqatala",
+      description: "Details about Zaqatala",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1734: {
+      name: "Xocavənd",
+      description: "Details about Xocavənd",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1735: {
+      name: "Lankaran",
+      description: "Details about Lankaran",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1736: {
+      name: "Qubadli",
+      description: "Details about Qubadli",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1737: {
+      name: "Şuşa",
+      description: "Details about Şuşa",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1738: {
+      name: "Tərtər",
+      description: "Details about Tərtər",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1739: {
+      name: "Xocalı",
+      description: "Details about Xocalı",
+      url: "/pages/meat-processing.html"
+    },
+    AZE1740: {
+      name: "Zəngilan",
+      description: "Details about Zəngilan",
+      url: "/pages/meat-processing.html"
+    },
+    AZE2415: {
+      name: "Sədərək",
+      description: "Details about Sədərək",
+      url: "/pages/meat-processing.html"
+    },
+    AZE2418: {
+      name: "Ordubad",
+      description: "Details about Ordubad",
+      url: "/pages/meat-processing.html"
+    },
+    AZE2419: {
+      name: "Şərur",
+      description: "Details about Şərur",
+      url: "/pages/meat-processing.html"
+    },
+    AZE2420: {
+      name: "Babək",
+      description: "Details about Babək",
+      url: "/pages/meat-processing.html"
+    },
+    AZE2421: {
+      name: "Culfa",
+      description: "Details about Culfa",
+      url: "/pages/meat-processing.html"
+    },
+    AZE2422: {
+      name: "Naxçıvan",
+      description: "Details about Naxçıvan",
+      url: "/pages/meat-processing.html"
+    },
+    AZE2423: {
+      name: "Şahbuz",
+      description: "Details about Şahbuz",
+      url: "/pages/meat-processing.html"
+    },
+    AZE4838: {
+      name: "Stepanakert",
+      description: "Details about Stepanakert",
+      url: "/pages/meat-processing.html"
+    },
+    AZE5561: {
+      name: "Naftalan",
+      description: "Details about Naftalan",
+      url: "/pages/meat-processing.html"
+    },
+    AZE5562: {
+      name: "Lankaran",
+      description: "Details about Lankaran",
+      url: "/pages/meat-processing.html"
+    },
+    AZE5563: {
+      name: "Shirvan",
+      description: "Details about Shirvan",
+      url: "/pages/meat-processing.html"
+    },
+    AZE5564: {
+      name: "Şəki",
+      description: "Details about Şəki",
+      url: "/pages/meat-processing.html"
+    },
+    AZE5565: {
+      name: "Şuşa",
+      description: "Details about Şuşa",
+      url: "/pages/meat-processing.html"
+    },
+    AZE5566: {
+      name: "Yevlakh",
+      description: "Details about Yevlakh",
+      url: "/pages/meat-processing.html"
+    },
+    AZE5567: {
+      name: "Kangarli",
+      description: "Details about Kangarli",
+      url: "/pages/meat-processing.html"
+    }
+  },
+  locations: {
+  },
+  labels: {},
+  legend: {
+    entries: []
+  },
+  regions: {}
+};
