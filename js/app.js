8d4a99748a69f620632d5cbf661b9800e64c7ea7
Orkhan Huseynli <Orkhan.Huseynli@vmedia.ca>
2024-09-17 00:25:20 +0400

    update

--- js/app.js
+++ js/app.js
@@ -4,18 +4,18 @@ document.addEventListener("DOMContentLoaded", () => {
   const mobileNavbar = document.getElementById("mobileNavbar");
   const preloader = document.getElementById("preloader");
 
-  if (!sessionStorage.getItem("preloader")) {
-    preloader.style.display = "flex";
-    setTimeout(() => {
-      preloader.style.display = "none";
-    }, 1000);
-  }
+  // if (!sessionStorage.getItem("preloader")) {
+  //   preloader.style.display = "flex";
+  //   setTimeout(() => {
+  //     preloader.style.display = "none";
+  //   }, 1000);
+  // }
 
-  sessionStorage.setItem("preloader", true);
-  hamburgerIcon.addEventListener("click", () => {
-    mobileNavbar.classList.remove("translate-x-[100vw]");
-    mobileNavbar.classList.add("translate-x-0");
-  });
+  // sessionStorage.setItem("preloader", true);
+  // hamburgerIcon.addEventListener("click", () => {
+  //   mobileNavbar.classList.remove("translate-x-[100vw]");
+  //   mobileNavbar.classList.add("translate-x-0");
+  // });
 
   closeIcon.addEventListener("click", () => {
     mobileNavbar.classList.add("translate-x-[100vw]");
