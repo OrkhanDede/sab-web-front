4e2018d5bfb635c8e149ab38b10a18d1f222f0b8
Orkhan Huseynli <Orkhan.Huseynli@vmedia.ca>
2024-09-19 00:14:35 +0400

    update

--- js/preloader.js
+++ js/preloader.js
@@ -1,23 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
   const preloader = document.getElementById("preloader");
-  const preloaderLogo = document.querySelector("#preloader .preloader-logo");
-
-  const isFirstTime = !sessionStorage.getItem("preloaderShown");
-
-  if (isFirstTime) {
-    preloaderLogo.classList.add("preloader-logo-animation", "w-0");
-  } else {
-    preloaderLogo.classList.add("w-[15rem]");
-  }
-
   window.onload = function () {
-    if (isFirstTime) {
-      setTimeout(() => {
-        preloader.classList.add("preloader-fade-out");
-      }, 2000);
-      sessionStorage.setItem("preloaderShown", true);
-    } else {
-      preloader.classList.add("preloader-fade-out");
-    }
+    preloader.classList.add("preloader-fade-out");
   };
 });
+
+
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   const preloader = document.getElementById("preloader");
+//   const preloaderLogo = document.querySelector("#preloader .preloader-logo");
+
+//   const isFirstTime = !sessionStorage.getItem("preloaderShown");
+
+//   if (isFirstTime) {
+//     preloaderLogo.classList.add("preloader-logo-animation", "w-0");
+//   } else {
+//     preloaderLogo.classList.add("w-[15rem]");
+//   }
+
+//   window.onload = function () {
+//     if (isFirstTime) {
+//       setTimeout(() => {
+//         preloader.classList.add("preloader-fade-out");
+//       }, 2000);
+//       sessionStorage.setItem("preloaderShown", true);
+//     } else {
+//       preloader.classList.add("preloader-fade-out");
+//     }
+//   };
+// });
