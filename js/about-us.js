92b55f14fcefc7480b51908a7bbeac5951255426
Orkhan Huseynli <Orkhan.Huseynli@vmedia.ca>
2024-09-20 17:04:45 +0400

    update meat

--- js/about-us.js
+++ js/about-us.js
@@ -353,14 +353,6 @@ document.addEventListener("DOMContentLoaded", () => {
       const section = destination.index;
       window.history.replaceState(null, null, `#${destination.anchor}`);
 
-      // if (section === 2 && !slideMounted) {
-      //   slideMounted = true;
-      //   historySplide.mount();
-      //   yearSplide.mount();
-      // }
-      // if (section === 4 + +isMobile && !swiperInstance) {
-      //   swiperInstance =
-      // }
     },
   });
 
@@ -372,7 +364,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const defaultTextClass = "text-secondary";
   const defaultRedColor = "#EE3239";
   const grayColor = "#7A7878";
-  const meetTitles = document.querySelectorAll(".meat-titles");
+  const meetTitles = document.querySelectorAll(".meat-title");
   const mobileTitles = document.querySelectorAll(".mobile_meat_title");
 
   mobileTitles.forEach((title) => {
@@ -392,7 +384,7 @@ document.addEventListener("DOMContentLoaded", () => {
       });
 
       title.classList.add("active-mobile-meat-type");
-      const selectedTitle = this.querySelector(".meat-titles");
+      const selectedTitle = this.querySelector(".");
       selectedTitle.classList.add("active-mobile-line");
       selectedTitle.style.color = defaultRedColor;
 
@@ -410,9 +402,8 @@ document.addEventListener("DOMContentLoaded", () => {
   document.querySelectorAll(".meet-option").forEach((option) => {
     option.addEventListener("mouseenter", function () {
       const newSrc = this.getAttribute("data-img-src");
-      const meatType = this.getAttribute("data-meat-type");
       meatImage.src = newSrc;
-      meatImage.className = `w-full lg:pl-[21rem] px-10 rounded-full h-full meat-section-image ${meatType}`;
+      //meatImage.className = `w-full lg:pl-[21rem] px-10 rounded-full h-full meat-section-image ${meatType}`;
 
       meetTitles.forEach((title) => {
         if (title) {
@@ -421,12 +412,9 @@ document.addEventListener("DOMContentLoaded", () => {
         }
       });
 
-      const title = this.querySelector(".meat-titles");
+      const title = this.querySelector(".meat-title");
       title.classList.add(defaultTextClass);
       title.style.color = defaultRedColor;
-
-      const line = this.querySelector(".line");
-      line.style.backgroundColor = defaultRedColor;
     });
 
     option.addEventListener("mouseleave", function () {
@@ -451,24 +439,4 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 
-  document
-    .querySelector(".meet_section_info")
-    .addEventListener("mouseleave", function () {
-      meatImage.src = defaultImageSrc;
-      meatImage.className = `w-full lg:pl-[21rem] px-10 rounded-full h-full meat-section-image meat`;
-
-      meetTitles.forEach((title) => {
-        title.classList.remove(defaultTextClass);
-        title.style.color = grayColor;
-      });
-      document.querySelector(".meet").classList.add(defaultTextClass);
-      document.querySelector(".meet").style.color = defaultRedColor;
-
-      document
-        .querySelectorAll(".indicator")
-        .forEach((ind) => (ind.style.borderColor = grayColor));
-      document
-        .querySelectorAll(".line")
-        .forEach((ln) => (ln.style.backgroundColor = grayColor));
-    });
 });
